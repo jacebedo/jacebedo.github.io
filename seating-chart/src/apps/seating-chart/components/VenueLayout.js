d69dd2a90f118339a0396bde7ea5a7528fb32109
J.P. Acebedo <acebedo.jp@gmail.com>
2025-06-11 22:02:00 -0700

    Add map animation

--- seating-chart/src/apps/seating-chart/components/VenueLayout.js
+++ seating-chart/src/apps/seating-chart/components/VenueLayout.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 import venueMap from '../assets/Venue.png';
 import ImageMarker from 'react-image-marker';
@@ -9,13 +9,13 @@ const styles = {
   }
 }
 
-const CustomMarker = (props) => {
-  return <div className="custom-marker"></div>
-}
+const CustomMarker = React.forwardRef((props, ref) => {
+  return <div ref={ref} className="custom-marker"></div>
+});
 
 export default function VenueLayout({selectedGuest}) {
   const [marker, setMarker] = useState([]);
-
+  const markerRef = useRef(null);
   useEffect(() => {
     if (selectedGuest){
       setMarker([{top: selectedGuest.y, left: selectedGuest.x}])
@@ -28,13 +28,19 @@ export default function VenueLayout({selectedGuest}) {
     <div className="justify-content-center align-items-center w-100 h-auto p-3">
       <div style={styles.venueMapContainer}>
         <TransformWrapper>
-          <TransformComponent>
-            {/* <img ref={imgRef} src={venueMap} alt="Venue Layout" className="img-fluid" />
-            {selectedGuest && (<Circle x={selectedGuest.x} y={selectedGuest.y} xScale={xScale} yScale={yScale} r={5} color="red" />)} */}
-            <ImageMarker src={venueMap} markers={marker} markerComponent={CustomMarker}/>
-            <div className="overlay-buttons">
-            </div>
-          </TransformComponent>
+          {({ zoomIn, zoomOut, resetTransform, zoomToElement, ...rest }) => (
+            <>
+              <div className="tools">
+                <button onClick={() => zoomIn()}>+</button>
+                <button onClick={() => zoomOut()}>-</button>
+                <button onClick={() => {console.log(markerRef); zoomToElement(markerRef.current, 3, 1, 'easeOut')}}>x</button>
+              </div>
+              <TransformComponent>
+                  <ImageMarker src={venueMap} markers={marker} markerComponent={(props) => <CustomMarker ref={markerRef} {...props}/>}/>
+              </TransformComponent>
+            </>
+          )}
+
         </TransformWrapper>
       </div>
       
